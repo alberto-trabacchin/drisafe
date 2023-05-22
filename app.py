0da33cb633f3ba43e94f0f6d8f867738bd86fc84
Alberto Trabacchin <alberto.trabacchin@gmail.com>
2023-05-22 14:12:44 +0900

    Project setted up with drisafe main package and tests.

---
+++ app.py
@@ -0,0 +1,4 @@
+from drisafe import constants
+
+if __name__ == "__main__":
+    print(constants.DS_PATH)
---
+++ drisafe/constants.py
@@ -0,0 +1,7 @@
+# constants.py
+
+"""This module defines project-level constants."""
+
+import pathlib
+
+DS_PATH = pathlib.Path("F:/Veoneer/Datasets/Dr(eye)ve/DREYEVE_DATA")
