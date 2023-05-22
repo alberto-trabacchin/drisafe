1992f8cd6c3a961c1c62b2db0c4ed6c7d4d4abb6
Alberto Trabacchin <alberto.trabacchin@gmail.com>
2023-05-22 14:52:50 +0900

    Added read_image(path) to read image with unicode path.

--- drisafe/constants.py
+++ drisafe/constants.py
@@ -4,4 +4,12 @@
 
 import pathlib
 
+REC = "06"
 DS_PATH = pathlib.Path("F:/Veoneer/Datasets/Dr(eye)ve/DREYEVE_DATA")
+RT_VID_PATH = DS_PATH / REC / "video_garmin.avi"
+ETG_VID_PATH = DS_PATH / REC / "video_etg.avi"
+RT_SAMPLE = pathlib.Path().absolute() / "media" / "rt_camera_sample.png"
+ETG_SAMPLE = pathlib.Path().absolute() / "media" / "etg_camera_sample.png"
+
+if __name__ == "__main__":
+    print(f"{RT_SAMPLE}: {RT_SAMPLE.is_file()}")
--- drisafe/homography.py
+++ drisafe/homography.py
@@ -1,1 +1,18 @@
 import numpy as np
+import cv2 as cv
+import pathlib
+from constants import RT_SAMPLE, ETG_SAMPLE
+
+def read_image(path):
+    img = cv.imdecode(np.fromfile(path, dtype = np.uint8), cv.IMREAD_UNCHANGED)
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    return img_gray, img_rgb
+
+def SIFT(img):
+    sift_detector = cv.SIFT_create()
+    kp, des = sift_detector.detectAndCompute(img, None)
+    return kp, des
+
+if __name__ == "__main__":
+    img_gray, img_rgb = read_image(RT_SAMPLE)
