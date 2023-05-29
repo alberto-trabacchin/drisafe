187066792dbd81a627725b40150f8e6443d1f1a3
Alberto Trabacchin <alberto.trabacchin@gmail.com>
2023-05-29 16:25:47 +0900

    Removed ds_load module.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from drisafe import homography
 if __name__ == "__main__":
     sensors = SensorStreams(SENSORS)
     while True:
-        sensors.read(show_frames = True, show_gaze = True)
+        sensors.read(show_frames = False, show_gaze = True)
         if not sensors.online: break
-        sensors.calc_rt_coords(verbose = True)
+        sensors.update_rt_coords(show_hom_info = False, show_proj_gaze = False,
+                                 plot_proj_gaze = False, plot_matches = False)
