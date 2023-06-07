a3f9bd3c461a0655450223edafb6657b8ebf9581
Alberto Trabacchin <alberto.trabacchin@gmail.com>
2023-06-07 13:36:51 +0900

    All recordings included in SENSORS structure.

--- drisafe/constants.py
+++ drisafe/constants.py
@@ -4,12 +4,12 @@
 
 import pathlib
 
-REC = "06"
 DS_PATH = pathlib.Path("F:/Veoneer/Datasets/Dr(eye)ve/DREYEVE_DATA")
-RT_VID_PATH = DS_PATH / REC / "video_garmin.avi"
-ETG_VID_PATH = DS_PATH / REC / "video_etg.avi"
-ETG_DATA_PATH = DS_PATH / REC / "etg_samples.txt"
-GPS_PATH = DS_PATH / REC / "speed_course_coord.txt"
+_recordings = [d for d in list(DS_PATH.iterdir()) if d.is_dir()]
+RT_VID_PATHS = [s / "video_garmin.avi" for s in _recordings]
+ETG_VID_PATHS = [s / "video_etg.avi" for s in _recordings]
+ETG_DATA_PATHS = [s / "etg_samples.txt" for s in _recordings]
+GPS_PATHS = [s / "speed_course_coord.txt" for s in _recordings]
 RT_SAMPLE_PATH = pathlib.Path().absolute() / "media" / "rt_camera_sample_5.png"
 ETG_SAMPLE_PATH = pathlib.Path().absolute() / "media" / "etg_camera_sample_5.png"
 
@@ -19,25 +19,34 @@ FPS_ETG = 30
 SENSORS = {
     "gaze_track": {
         "name": "Gaze Tracker",
-        "path": ETG_DATA_PATH
+        "paths": ETG_DATA_PATHS
     },
     "etg_cam": {
         "name": "ETG Camera",
-        "path": ETG_VID_PATH,
+        "paths": ETG_VID_PATHS,
         "fps": FPS_ETG
     },
     "roof_cam": {
         "name": "Roof Top Camera",
-        "path": RT_VID_PATH,
+        "paths": RT_VID_PATHS,
         "fps": FPS_RT
     }
 }
 
+def _check_all(path_list, dtype = "file"):
+    if dtype == "file":
+        return all([f.is_file() for f in path_list])
+    elif dtype == "folder":
+        return all([f.is_dir() for f in path_list])
+    else:
+        return False
+
 if __name__ == "__main__":
-    print(f"{DS_PATH}: {DS_PATH.is_dir()}")
-    print(f"{RT_VID_PATH}: {RT_VID_PATH.is_file()}")
-    print(f"{ETG_VID_PATH}: {ETG_VID_PATH.is_file()}")
-    print(f"{ETG_DATA_PATH}: {ETG_DATA_PATH.is_file()}")
-    print(f"{GPS_PATH}: {GPS_PATH.is_file()}")
-    print(f"{RT_SAMPLE_PATH}: {RT_SAMPLE_PATH.is_file()}")
-    print(f"{ETG_SAMPLE_PATH}: {ETG_SAMPLE_PATH.is_file()}")
+    print(f"DS main dir: {DS_PATH.is_dir()}")
+    print(f"Samples: {_check_all(_recordings, 'folder')}")
+    print(f"RT videos: {_check_all(RT_VID_PATHS, 'file')}")
+    print(f"ETG videos: {_check_all(ETG_VID_PATHS, 'file')}")
+    print(f"ETG data: {_check_all(ETG_DATA_PATHS, 'file')}")
+    print(f"GPS data: {_check_all(GPS_PATHS, 'file')}")
+    print(f"RT image sample: {_check_all([RT_SAMPLE_PATH], 'file')}")
+    print(f"ETG image sample: {_check_all([ETG_SAMPLE_PATH], 'file')}")
