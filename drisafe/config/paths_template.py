f69a04580bf088792126aab18338a94f57bfaeec
Alberto Trabacchin <alberto.trabacchin@gmail.com>
2023-06-16 11:10:23 +0900

    Configuration information added to dedicated configuration file

---
+++ drisafe/config/paths_template.py
@@ -0,0 +1,29 @@
+import pathlib
+
+DS_PATH = pathlib.Path("C:/Users/Alberto/Desktop/Datasets/Dr(eye)ve/DREYEVE_DATA")
+_recordings = [d for d in list(DS_PATH.iterdir()) if d.is_dir()]
+RT_VID_PATHS = [s / "video_garmin.avi" for s in _recordings]
+ETG_VID_PATHS = [s / "video_etg.avi" for s in _recordings]
+ETG_DATA_PATHS = [s / "etg_samples.txt" for s in _recordings]
+ETG_PROJ_DATA_PATHS = [s / "etg_proj_samples.csv" for s in _recordings]
+GPS_PATHS = [s / "speed_course_coord.txt" for s in _recordings]
+RT_SAMPLE_PATH = pathlib.Path().absolute() / "media" / "rt_camera_sample_5.png"
+ETG_SAMPLE_PATH = pathlib.Path().absolute() / "media" / "etg_camera_sample_5.png"
+
+def _check_all(path_list, dtype = "file"):
+    if dtype == "file":
+        return all([f.is_file() for f in path_list])
+    elif dtype == "folder":
+        return all([f.is_dir() for f in path_list])
+    else:
+        return False
+
+if __name__ == "__main__":
+    print(f"DS main dir: {DS_PATH.is_dir()}")
+    print(f"Samples: {_check_all(_recordings, 'folder')}")
+    print(f"RT videos: {_check_all(RT_VID_PATHS, 'file')}")
+    print(f"ETG videos: {_check_all(ETG_VID_PATHS, 'file')}")
+    print(f"ETG data: {_check_all(ETG_DATA_PATHS, 'file')}")
+    print(f"GPS data: {_check_all(GPS_PATHS, 'file')}")
+    print(f"RT image sample: {_check_all([RT_SAMPLE_PATH], 'file')}")
+    print(f"ETG image sample: {_check_all([ETG_SAMPLE_PATH], 'file')}")
--- drisafe/constants.py
+++ drisafe/constants.py
@@ -2,17 +2,7 @@
 
 """This module defines project-level constants."""
 
-import pathlib
-
-DS_PATH = pathlib.Path("C:/Users/Alberto/Desktop/Datasets/Dr(eye)ve/DREYEVE_DATA")
-_recordings = [d for d in list(DS_PATH.iterdir()) if d.is_dir()]
-RT_VID_PATHS = [s / "video_garmin.avi" for s in _recordings]
-ETG_VID_PATHS = [s / "video_etg.avi" for s in _recordings]
-ETG_DATA_PATHS = [s / "etg_samples.txt" for s in _recordings]
-ETG_PROJ_DATA_PATHS = [s / "etg_proj_samples.csv" for s in _recordings]
-GPS_PATHS = [s / "speed_course_coord.txt" for s in _recordings]
-RT_SAMPLE_PATH = pathlib.Path().absolute() / "media" / "rt_camera_sample_5.png"
-ETG_SAMPLE_PATH = pathlib.Path().absolute() / "media" / "etg_camera_sample_5.png"
+from drisafe.config import paths
 
 FPS_RT = 25
 FPS_ETG = 30
@@ -21,37 +11,21 @@ SENSORS = [
     {
         "gaze_track": {
         "name": "Gaze Tracker",
-        "etg_crd_path": ETG_DATA_PATHS[i],
-        "rt_crd_path": ETG_PROJ_DATA_PATHS[i]
+        "etg_crd_path": paths.ETG_DATA_PATHS[i],
+        "rt_crd_path": paths.ETG_PROJ_DATA_PATHS[i]
         },
         "etg_cam": {
         "name": "ETG Camera",
-        "vid_path": ETG_VID_PATHS[i],
+        "vid_path": paths.ETG_VID_PATHS[i],
         "fps": FPS_ETG
         },
         "roof_cam": {
         "name": "Roof Top Camera",
-        "vid_path": RT_VID_PATHS[i],
+        "vid_path": paths.RT_VID_PATHS[i],
         "fps": FPS_RT
         }
     }
-    for i in range(len(_recordings))]
-
-def _check_all(path_list, dtype = "file"):
-    if dtype == "file":
-        return all([f.is_file() for f in path_list])
-    elif dtype == "folder":
-        return all([f.is_dir() for f in path_list])
-    else:
-        return False
+    for i in range(len(paths._recordings))]
 
 if __name__ == "__main__":
-    #print(f"DS main dir: {DS_PATH.is_dir()}")
-    #print(f"Samples: {_check_all(_recordings, 'folder')}")
-    #print(f"RT videos: {_check_all(RT_VID_PATHS, 'file')}")
-    #print(f"ETG videos: {_check_all(ETG_VID_PATHS, 'file')}")
-    #print(f"ETG data: {_check_all(ETG_DATA_PATHS, 'file')}")
-    #print(f"GPS data: {_check_all(GPS_PATHS, 'file')}")
-    #print(f"RT image sample: {_check_all([RT_SAMPLE_PATH], 'file')}")
-    #print(f"ETG image sample: {_check_all([ETG_SAMPLE_PATH], 'file')}")
     print(SENSORS[0])
